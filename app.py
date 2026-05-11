43a6d893ce11c4b63f735a452595bdcdf431c191
Tanisha Soni <stanisha585@gmail.com>
2026-05-11 23:10:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,30 @@ sex = st.selectbox("Sex", ["male", "female"])
 
 age = st.slider("Age", 1, 80, 25)
 
+sibsp = st.slider("Siblings/Spouses Aboard", 0, 10, 0)
+
+parch = st.slider("Parents/Children Aboard", 0, 10, 0)
+
 fare = st.slider("Fare", 0, 500, 50)
 
-sex = 1 if sex == "male" else 0
+embarked = st.selectbox("Embarked", ["C", "Q", "S"])
+
+sex = 0 if sex == "male" else 1
+
+embarked_c = 1 if embarked == "C" else 0
+embarked_q = 1 if embarked == "Q" else 0
+embarked_s = 1 if embarked == "S" else 0
 
 input_data = pd.DataFrame({
     "Pclass": [pclass],
+    "Sex": [sex],
     "Age": [age],
+    "SibSp": [sibsp],
+    "Parch": [parch],
     "Fare": [fare],
-    "Sex_male": [sex]
+    "Embarked_C": [embarked_c],
+    "Embarked_Q": [embarked_q],
+    "Embarked_S": [embarked_s]
 })
 
 if st.button("Predict"):
