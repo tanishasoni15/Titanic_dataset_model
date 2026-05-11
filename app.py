315409aa69cbd5e336b7bc04ebc2e6f742ea3289
Tanisha Soni <stanisha585@gmail.com>
2026-05-11 23:08:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ model = pickle.load(open("titanic_model.pkl", "rb"))
 st.title("Titanic Survival Prediction")
 
 pclass = st.selectbox("Passenger Class", [1, 2, 3])
+
 sex = st.selectbox("Sex", ["male", "female"])
+
 age = st.slider("Age", 1, 80, 25)
+
 fare = st.slider("Fare", 0, 500, 50)
 
 sex = 1 if sex == "male" else 0
@@ -21,9 +24,10 @@ input_data = pd.DataFrame({
 })
 
 if st.button("Predict"):
+
     prediction = model.predict(input_data)
 
     if prediction[0] == 1:
-        st.success("Passenger Survived")
+        st.success("Passenger Survived ✅")
     else:
-        st.error("Passenger Did Not Survive")
+        st.error("Passenger Did Not Survive ❌")
